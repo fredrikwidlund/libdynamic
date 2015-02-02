94eb6e8cfcc1fe790b1d0ffd3878342f3b9377a0
Fredrik Widlund <fredrik.widlund@gmail.com>
2015-02-02 11:06:16 +0000

    Update dynamic.h to reflect on previous change

--- src/dynamic.h
+++ src/dynamic.h
@@ -1,13 +1,15 @@
 #ifndef DYNAMIC_H_INCLUDED
 #define DYNAMIC_H_INCLUDED
 
-#define DYNAMIC_VERSION "0.2.0"
+#define DYNAMIC_VERSION "0.3.0"
 #define DYNAMIC_VERSION_MAJOR 0
-#define DYNAMIC_VERSION_MINOR 2
+#define DYNAMIC_VERSION_MINOR 3
 #define DYNAMIC_VERSION_PATCH 0
 
 #include <dynamic/buffer.h>
 #include <dynamic/vector.h>
 #include <dynamic/string.h>
+#include <dynamic/map_int.h>
+#include <dynamic/map_str.h>
 
 #endif /* DYNAMIC_H_INCLUDED */
