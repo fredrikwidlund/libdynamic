1581c370481d54f1c5ce2511ae757975749db491
Fredrik Widlund <fredrik.widlund@gmail.com>
2017-01-02 22:00:37 +0000

    Rip mods

--- docs/conf.py
+++ docs/conf.py
@@ -40,15 +40,15 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Jansson'
-copyright = u'2009-2016, Petri Lehtinen'
+project = u'libdynamic'
+copyright = u'2017, Fredrik Widlund'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '2.10-dev'
+version = '1.0-dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -165,7 +165,7 @@ pygments_style = 'sphinx'
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Janssondoc'
+htmlhelp_basename = 'libreactordoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -179,8 +179,8 @@ htmlhelp_basename = 'Janssondoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Jansson.tex', u'Jansson Documentation',
-   u'Petri Lehtinen', 'manual'),
+  ('index', 'libreactor.tex', u'libreactor Documentation',
+   u'Fredrik Widlund', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -212,6 +212,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'jansson', u'Jansson Documentation',
-     [u'Petri Lehtinen'], 1)
+    ('index', 'libreactor', u'libreactor Documentation',
+     [u'Fredrik Widlund'], 1)
 ]
--- docs/index.rst
+++ docs/index.rst
@@ -1,13 +1,12 @@
-Jansson Documentation
+libreactor Documentation
 =====================
 
-This is the documentation for Jansson_ |release|, last updated |today|.
+This is the documentation for libreactor_ |release|, last updated |today|.
 
 Introduction
 ------------
 
-Jansson_ is a C library for encoding, decoding and manipulating JSON
-data. Its main features and design principles are:
+libreactor_ is a C library for blah blah ... Its main features and design principles are:
 
 - Simple and intuitive API and data model
 
@@ -19,17 +18,10 @@ data. Its main features and design principles are:
 
 - Extensive test suite
 
-Jansson is licensed under the `MIT license`_; see LICENSE in the
+libreactor is licensed under the `MIT license`_; see LICENSE in the
 source distribution for details.
 
-Jansson is used in production and its API is stable. It works on
-numerous platforms, including numerous Unix like systems and Windows.
-It's suitable for use on any system, including desktop, server, and
-small embedded systems.
-
-
 .. _`MIT license`: http://www.opensource.org/licenses/mit-license.php
-.. _Jansson: http://www.digip.org/jansson/
 
 Contents
 --------
@@ -37,13 +29,7 @@ Contents
 .. toctree::
    :maxdepth: 2
 
-   gettingstarted
-   upgrading
-   tutorial
-   conformance
-   portability
-   apiref
-   changes
+   bleh
 
 
 Indices and Tables
