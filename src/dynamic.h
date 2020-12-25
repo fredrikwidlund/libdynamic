7a809c5e9166faeb3cd4742699d4b5584ac1f947
Fredrik Widlund <fredrik.widlund@gmail.com>
2020-12-25 18:07:09 +0100

    bump minor

--- src/dynamic.h
+++ src/dynamic.h
@@ -1,9 +1,9 @@
 #ifndef DYNAMIC_H_INCLUDED
 #define DYNAMIC_H_INCLUDED
 
-#define DYNAMIC_VERSION "2.1.0"
+#define DYNAMIC_VERSION "2.2.0"
 #define DYNAMIC_VERSION_MAJOR 2
-#define DYNAMIC_VERSION_MINOR 1
+#define DYNAMIC_VERSION_MINOR 2
 #define DYNAMIC_VERSION_PATCH 0
 
 #define dynamic_likely(x)   __builtin_expect(!!(x), 1)
